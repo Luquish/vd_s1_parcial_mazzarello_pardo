183c880a7599c0b647dceef303bbcc01ec88e117
Luquish <lucamazza02@gmail.com>
2023-04-18 10:22:10 -0300

    Fix(horizontal.js)

--- src/horizontal.js
+++ src/horizontal.js
@@ -1,49 +1,81 @@
-d3.dsv(';', './data/mosquitos_2022.csv', d3.autoType).then((data) => {
+// d3.dsv(';', './data/mosquitos_2022.csv', d3.autoType).then((data) => {
 
-    // set the dimensions and margins of the graph
-    marginLeft: 100;
-    marginRight: 100;
-    marginTop: 100;
-    marginBottom: 100;
-    width: 1000;
-    height: 1000;
+//     console.log(data);
+  
+//     var count = {};
+  
+//     // count how much each canal occurs in list and store in count
+//     data.forEach(function(d) {
+//       var canal = d.canal;
+//       if(count[canal] === undefined) {
+//         count[canal] = 1;
+//       } else {
+//         count[canal] += 1;
+//       }
+//     });
+  
+//     // now store the count in each data member
+//     data.forEach(function(d) {
+//       var canal = d.canal;
+//       d.count = count[canal];
+//     });
+  
+//     // sort data by count
+//     data.sort(function(a, b) {
+//       return b.count - a.count;
+//     });
+  
+//     console.log(count);
+  
+//     // Guardamos el svg generado en la variable chart
+//     let chart = Plot.plot({
+//         width: 600,
+//         height: 400,
+//         grid: true,
+//         marks: [
+//         Plot.barX(data, Plot.groupY({x: "count"}, {y: "canal", sort: {y: "x", reverse: true}})),
+//       ],
+//     });
 
-    console.log(data);
+//     d3.select('#chart').html('').append(() => chart);
+//   });
   
-    var count = {};
-  
-    // count how much each canal occurs in list and store in count
-    data.forEach(function(d) {
-      var canal = d.canal;
-      if(count[canal] === undefined) {
-        count[canal] = 1;
-      } else {
-        count[canal] += 1;
-      }
-    });
-  
-    // now store the count in each data member
-    data.forEach(function(d) {
-      var canal = d.canal;
-      d.count = count[canal];
-    });
-  
-    // sort data by count
-    data.sort(function(a, b) {
-      return b.count - a.count;
-    });
-  
-    console.log(count);
-  
-    // Guardamos el svg generado en la variable chart
-    let chart = Plot.plot({
-      grid: true,
-      marks: [
-        Plot.barX(data, Plot.groupY({x: "count"}, {y: "canal", sort: {y: "x", reverse: true}})),
-      ],
-    });
-
+  function PlotHorizontal(dataPromise, divId) {
+    dataPromise.then((data) => {
+      var count = {};
     
-    d3.select('#chart').html('').append(() => chart);
-  });
+      // count how much each canal occurs in list and store in count
+      data.forEach(function(d) {
+        var canal = d.canal;
+        if(count[canal] === undefined) {
+          count[canal] = 1;
+        } else {
+          count[canal] += 1;
+        }
+      });
+    
+      // now store the count in each data member
+      data.forEach(function(d) {
+        var canal = d.canal;
+        d.count = count[canal];
+      });
+    
+      // sort data by count
+      data.sort(function(a, b) {
+        return b.count - a.count;
+      });
+    
+      // Guardamos el svg generado en la variable chart
+      let chart = Plot.plot({
+        width: 600,
+        height: 400,
+        grid: true,
+        marks: [
+          Plot.barX(data, Plot.groupY({x: "count"}, {y: "canal", sort: {y: "x", reverse: true}})),
+        ],
+      });
+    
+      d3.select(divId).html('').append(() => chart);
+    });
+  }
   
